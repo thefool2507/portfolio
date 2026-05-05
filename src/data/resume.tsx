79ceea333dcf0feb0f2464e747e80a28342bd3cb
Fahreza Rizky <rizkyfahreza937@gmail.com>
2026-05-06 02:21:57 +0700

    update

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -230,32 +230,29 @@ export const DATA = {
   ],
   projects: [
     {
-      title: "Chat Collect",
-      href: "https://chatcollect.com",
-      dates: "Jan 2024 - Feb 2024",
+      title: "Fun Trip",
+      href: "https://funtrip-page.vercel.app/",
+      dates: "May 2026",
       active: true,
       description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+        "A modern travel landing page built with Next.js 16, Tailwind CSS v4, and TypeScript.",
       technologies: [
         "Next.js",
         "Typescript",
-        "PostgreSQL",
-        "Prisma",
+        "Lucide React",
+        "Next/Image",
         "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
       ],
       links: [
         {
           type: "Website",
-          href: "https://chatcollect.com",
+          href: "https://funtrip-page.vercel.app/",
           icon: <Icons.globe className="size-3" />,
         },
       ],
       image: "",
       video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+        "https://github.com/thefool2507/FunTrip_LandingPage/blob/main/public/FunTrip.mp4?raw=true",
     },
     {
       title: "Magic UI",
