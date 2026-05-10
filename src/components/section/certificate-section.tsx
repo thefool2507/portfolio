24f61657b916e88b964175827bd2801d3c69cc12
Fahreza Rizky Pradana <rizkyfahreza937@gmail.com>
2026-05-10 21:42:05 +0700

    Refactor certificate section layout and add content
    Updated the layout of the certificate section to improve structure and added a heading and description for achievements.

--- src/components/section/certificate-section.tsx
+++ src/components/section/certificate-section.tsx
@@ -184,14 +184,22 @@ export default function CertificateSection() {
     <section id="certificates">
       <div className="flex min-h-0 flex-col gap-y-6">
         <BlurFade delay={BLUR_FADE_DELAY * 14} inView>
-          <div className="flex items-center w-full">
-            <div className="flex-1 h-px bg-linear-to-r from-transparent from-5% via-border via-95% to-transparent" />
-            <div className="border bg-primary z-10 rounded-xl px-4 py-1">
-              <span className="text-background text-sm font-medium">
-                Certificates
-              </span>
+          <div className="flex flex-col gap-y-4 items-center justify-center">
+            <div className="flex items-center w-full">
+              <div className="flex-1 h-px bg-linear-to-r from-transparent from-5% via-border via-95% to-transparent" />
+              <div className="border bg-primary z-10 rounded-xl px-4 py-1">
+                <span className="text-background text-sm font-medium">
+                  Certificates
+                </span>
+              </div>
+              <div className="flex-1 h-px bg-linear-to-l from-transparent from-5% via-border via-95% to-transparent" />
             </div>
-            <div className="flex-1 h-px bg-linear-to-l from-transparent from-5% via-border via-95% to-transparent" />
+            <div className="flex flex-col gap-y-3 items-center justify-center">
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">Browse my achievements</h2>
+                <p className="text-muted-foreground md:text-lg/relaxed lg:text-base/relaxed xl:text-lg/relaxed text-balance text-center">
+                    Certifications and awards that showcase my journey of continuous learning and expertise in the field.
+                </p>
+            </div>
           </div>
         </BlurFade>
 
