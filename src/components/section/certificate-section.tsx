54c1d4476a1a30ce3cd284e4b8a6c4d1a8b7a790
Fahreza Rizky <rizkyfahreza937@gmail.com>
2026-05-19 05:01:06 +0700

    update

--- src/components/section/certificate-section.tsx
+++ src/components/section/certificate-section.tsx
@@ -195,7 +195,7 @@ export default function CertificateSection() {
               <div className="flex-1 h-px bg-linear-to-l from-transparent from-5% via-border via-95% to-transparent" />
             </div>
             <div className="flex flex-col gap-y-3 items-center justify-center">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">Browse my achievements</h2>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl text-center">Browse my achievements</h2>
                 <p className="text-muted-foreground md:text-lg/relaxed lg:text-base/relaxed xl:text-lg/relaxed text-balance text-center">
                     Certifications and awards that showcase my journey of continuous learning and expertise in the field.
                 </p>
